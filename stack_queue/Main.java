522ba368782b388abb70295042ed4805afc11e78
slee4736 <slee4736@uni.sydney.edu.au>
2024-01-15 15:54:15 +0900

    added queue implementations and tests

--- stack_queue/Main.java
+++ stack_queue/Main.java
@@ -2,6 +2,7 @@ package stack_queue;
 
 public class Main {
     public static void main(String[] args) {
+        // stack implementation test
         Stack stack = new Stack(5);
         stack.push(1);
         stack.push(2);
@@ -14,5 +15,20 @@ public class Main {
         System.out.println("Pop: " + stack.pop());
     
         System.out.println("Is stack empty? " + stack.isEmpty());
+
+        // queue implementation test
+        Queue queue = new Queue(5);
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println("Front element: " + queue.peek());
+        System.out.println("Queue size: " + queue.size());
+
+        System.out.println("Dequeue: " + queue.dequeue());
+        System.out.println("Dequeue: " + queue.dequeue());
+
+        System.out.println("Is queue empty? " + queue.isEmpty());
     }
 }
